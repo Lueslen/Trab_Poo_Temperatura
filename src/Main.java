41dd74cfd98f1700dc5404529264f6025e1b5b6f
Lueslen Machado da Silva <lueslensilva@hotmail.com>
2022-10-14 21:06:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
 import java.awt.*;
+
 import javax.swing.*;
 
 public class Main {
+    
     public static void main(String[] args) {
+        
         EventQueue.invokeLater(new Runnable() {
+            
             public void run() {
+                
                 try {
+                    
                     TempConverter frame = new TempConverter();
+                    
                     frame.setVisible(true);
+                    
                 } catch (Exception e) {
+                    
                     e.printStackTrace();
+                    
                 }
+                
             }
+            
         });
+        
     }
+    
 }
